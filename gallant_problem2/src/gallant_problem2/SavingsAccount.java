a9976a998579c81c4d242a540ebf8f631ed41c7c
DarthofLuke <gallant.thomas@outlook.com>
2020-02-22 20:34:54 -0500

    Finished problem 2

--- gallant_problem2/src/gallant_problem2/SavingsAccount.java
+++ gallant_problem2/src/gallant_problem2/SavingsAccount.java
@@ -2,9 +2,23 @@ package gallant_problem2;
 
 public class SavingsAccount {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	private static double annualIntrestRate;
+	
+	private double savingsBalance;
+	
+	public SavingsAccount(double balance) {
+		this.savingsBalance = balance;
 	}
-
+	
+	public double getSavings() {
+		return savingsBalance;
+	}
+	public void calculateMonthlyIntrest() {
+		savingsBalance = savingsBalance + (savingsBalance * annualIntrestRate / 12.0);
+	}
+	
+	public static void modifyIntrestRate(double newInterestRate) {
+		annualIntrestRate = newInterestRate;
+	}
 }
--- gallant_problem2/src/gallant_problem2/SavingsAccountTest.java
+++ gallant_problem2/src/gallant_problem2/SavingsAccountTest.java
@@ -3,7 +3,31 @@ package gallant_problem2;
 public class SavingsAccountTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		SavingsAccount saver1 = new SavingsAccount(2000);
+		SavingsAccount saver2 = new SavingsAccount(3000);
+		
+		SavingsAccount.modifyIntrestRate(0.04);
+		
+		for (int i = 1; i < 13; i++) {
+			saver1.calculateMonthlyIntrest();
+			saver2.calculateMonthlyIntrest();
+			double saver1Savings = saver1.getSavings();
+			double saver2Savings = saver2.getSavings();
+			System.out.printf("%.2f$ is the monthly balance for saver1 month %d\n", saver1Savings, i);
+			System.out.printf("%.2f$ is the monthly balance for saver2 month %d\n", saver2Savings, i);
+			System.out.println();
+		}
+		
+		
+		SavingsAccount.modifyIntrestRate(0.05);
+		
+		saver1.calculateMonthlyIntrest();
+		saver2.calculateMonthlyIntrest();
+		double saver1Savings = saver1.getSavings();
+		double saver2Savings = saver2.getSavings();
+		System.out.printf("%.2f$ is the monthly balance for saver1 month 13\n", saver1Savings);
+		System.out.printf("%.2f$ is the monthly balance for saver2 month 13\n", saver2Savings);
 
 	}
 
